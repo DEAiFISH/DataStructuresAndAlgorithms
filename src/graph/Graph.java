5c0b85db37efb368824dca6c9df8c4d1d236eb50
DEAIFISH <1277676309@qq.com>
2022-12-04 21:59:29 +0800

    2022/12/04

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -6,12 +6,12 @@ public class Graph {
     char[] vertexs;//图顶点名称
     int[][] matrix;//图关系矩阵
 
-    public Graph(char[] vertexs, char[][] edges){
-        size=vertexs.length;
-        matrix=new int[size][size];//设定图关系矩阵大小
-        this.vertexs=vertexs;
+    public Graph(char[] vertexs, char[][] edges) {
+        size = vertexs.length;
+        matrix = new int[size][size];//设定图关系矩阵大小
+        this.vertexs = vertexs;
 
-        for(char[] c:edges){//设置矩阵值
+        for (char[] c : edges) {//设置矩阵值
             int p1 = getPosition(c[0]);//根据顶点名称确定对应矩阵下标
             int p2 = getPosition(c[1]);
 
@@ -22,10 +22,10 @@ public class Graph {
     }
 
     //图的遍历输出
-    public void print(){
-        for(int[] i:matrix){
-            for(int j:i){
-                System.out.print(j+" ");
+    public void print() {
+        for (int[] i : matrix) {
+            for (int j : i) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
@@ -33,14 +33,14 @@ public class Graph {
 
     //根据顶点名称获取对应的矩阵下标
     private int getPosition(char ch) {
-        for(int i=0; i<vertexs.length; i++)
-            if(vertexs[i]==ch)
+        for (int i = 0; i < vertexs.length; i++)
+            if (vertexs[i] == ch)
                 return i;
         return -1;
     }
 
     public static void main(String[] args) {
-        char[] vexs = {'A', 'B', 'C', 'D', 'E', 'F', 'G','H','I','J','K'};
+        char[] vexs = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'};
         char[][] edges = new char[][]{
                 {'A', 'C'},
                 {'A', 'D'},
@@ -49,20 +49,20 @@ public class Graph {
                 {'C', 'D'},
                 {'E', 'G'},
                 {'D', 'G'},
-                {'I','J'},
-                {'J','G'},};
+                {'I', 'J'},
+                {'J', 'G'},};
         Graph pG;
         // 自定义"图"(输入矩阵队列)
         // 采用已有的"图"
-        long start=System.nanoTime();
+        long start = System.nanoTime();
 
-        for(int i=0;i<10000;i++){
+        for (int i = 0; i < 10000; i++) {
             pG = new Graph(vexs, edges);
 //            pG.print();   // 打印图
         }
 
-        long end=System.nanoTime();
+        long end = System.nanoTime();
 
-        System.out.println(end-start);
+        System.out.println(end - start);
     }
 }
